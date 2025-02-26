37741e36380c8d968e28d685f2b44b6c4316676b
Vikki <yingjialiu1216@gmail.com>
2025-02-25 19:31:36 -0800

    changed timer to 50 ms

--- typewriter.js
+++ typewriter.js
@@ -2,5 +2,5 @@ const sentence = "hello there from lighthouse labs";
 
 let timer = 0;
 for (const char of sentence) {
-  setTimeout(() => {process.stdout.write(char); console.log()}, timer += 1000)
+  setTimeout(() => {process.stdout.write(char); console.log()}, timer += 50)
 }
